719b26977b5d658e1181df14bc9561942c6511fc
unknown <iamrafsal97@gmail.com>
2020-10-20 20:11:05 +0530

    20/10/2020 Validation

---
+++ Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TextBox_Validation_MVC.Models
+{
+    public class PersonModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+    }
+}
